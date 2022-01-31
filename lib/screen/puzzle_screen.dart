781f08fa427f08d939be244cf5e4b00a17dc5e59
Zaldy Pagaduan <zaldypagaduanjr@gmail.com>
2022-01-31 18:33:11 +0800

    added hover implementation

--- lib/screen/puzzle_screen.dart
+++ lib/screen/puzzle_screen.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class PuzzleScreen extends StatefulWidget {
   const PuzzleScreen({Key? key}) : super(key: key);
@@ -12,9 +13,18 @@ class PuzzleScreen extends StatefulWidget {
 
 class _PuzzleScreenState extends State<PuzzleScreen> {
   List<int> tiles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  List<GlobalKey> keyList = [];
+  List<RenderBox> boxList = [];
+
+  createRenderBox() {
+    setState(() {
+      for (var element in keyList) {
+        boxList.add(element.currentContext!.findRenderObject() as RenderBox);
+      }
+    });
+  }
 
   moveTile(int index) {
-    //Checks if this tile can move left, up, right, down
     int whiteIndex = tiles.indexOf(16);
     if (index - 1 == whiteIndex && index % 4 != 0 ||
         index + 1 == whiteIndex && (index + 1) % 4 != 0 ||
@@ -27,42 +37,43 @@ class _PuzzleScreenState extends State<PuzzleScreen> {
     }
   }
 
-  isSolveable(List<int> puzzle) {
-    int parity = 0, row = 0, blankRow = 0;
-    double gridWidth = 4;
+  Set isSolveable(List<int> puzzle) {
+    int parity = 0, row = 5, blankRow = 0;
+    int gridWidth = sqrt(puzzle.length).toInt();
 
-    for (int i = 0; i < puzzle.length; i++) {
+    for (int i = 0; i < puzzle.length - 1; i++) {
       if (i % gridWidth == 0) {
-        row++;
+        row--;
       }
       if (puzzle[i] == 16) {
         blankRow = row;
-        continue;
       }
       for (int j = i + 1; j < puzzle.length; j++) {
-        if (puzzle[i] > puzzle[j] && puzzle[j] != 16) {
+        if (puzzle[i] > puzzle[j] && puzzle[j] != 16 && puzzle[i] != 16) {
           parity++;
         }
       }
     }
-    if (gridWidth % 2 == 0) {
-      if (blankRow % 2 == 0) {
-        return parity % 2 == 0;
+    if (gridWidth.isEven) {
+      if (blankRow.isEven) {
+        return {parity.isOdd, parity};
       } else {
-        return parity % 2 != 0;
+        return {parity.isEven, parity};
       }
     } else {
-      return parity % 2 == 0;
+      return {parity.isEven, parity};
     }
   }
 
   shuffle() {
     setState(() {
       tiles.shuffle();
-      bool solveable = isSolveable(tiles);
-      while (!solveable) {
+      int loop = 1;
+      Set solveable = isSolveable(tiles);
+      while (!solveable.first) {
         tiles.shuffle();
         solveable = isSolveable(tiles);
+        loop++;
       }
     });
   }
@@ -85,6 +96,16 @@ class _PuzzleScreenState extends State<PuzzleScreen> {
   void initState() {
     super.initState();
     shuffle();
+    if (!kIsWeb) {
+      setState(() {
+        keyList = List.generate(16, (index) => GlobalKey());
+      });
+    }
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      if (!kIsWeb) {
+        createRenderBox();
+      }
+    });
   }
 
   @override
@@ -97,16 +118,18 @@ class _PuzzleScreenState extends State<PuzzleScreen> {
             )),
       body: Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
             SizedBox(
-              width: 250,
-              height: 250,
+              width: 450,
+              height: 450,
               child: GridView.count(
                 shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
                 crossAxisCount: 4,
                 children: List.generate(tiles.length, (index) {
-                  return _buildTile(index);
+                  return kIsWeb
+                      ? _buildWebTile(index)
+                      : _buildMobileTile(index);
                 }),
               ),
             ),
@@ -116,16 +139,116 @@ class _PuzzleScreenState extends State<PuzzleScreen> {
     );
   }
 
-  _buildTile(int index) {
-    return InkWell(
-      child: Center(
-        child: Text(
-          "${tiles[index] == 16 ? "" : tiles[index]}",
+  _buildMobileTile(int index) {
+    return Listener(
+      key: keyList[index],
+      onPointerMove: (details) {
+        final result = BoxHitTestResult();
+        if (boxList[15].hitTest(
+          result,
+          position: boxList[15].globalToLocal(details.position),
+        )) {
+          moveTile(15);
+        } else if (boxList[14].hitTest(
+          result,
+          position: boxList[14].globalToLocal(details.position),
+        )) {
+          moveTile(14);
+        } else if (boxList[13].hitTest(
+          result,
+          position: boxList[13].globalToLocal(details.position),
+        )) {
+          moveTile(13);
+        } else if (boxList[12].hitTest(
+          result,
+          position: boxList[12].globalToLocal(details.position),
+        )) {
+          moveTile(12);
+        } else if (boxList[11].hitTest(
+          result,
+          position: boxList[11].globalToLocal(details.position),
+        )) {
+          moveTile(11);
+        } else if (boxList[10].hitTest(
+          result,
+          position: boxList[10].globalToLocal(details.position),
+        )) {
+          moveTile(10);
+        } else if (boxList[9].hitTest(
+          result,
+          position: boxList[9].globalToLocal(details.position),
+        )) {
+          moveTile(9);
+        } else if (boxList[8].hitTest(
+          result,
+          position: boxList[8].globalToLocal(details.position),
+        )) {
+          moveTile(8);
+        } else if (boxList[7].hitTest(
+          result,
+          position: boxList[7].globalToLocal(details.position),
+        )) {
+          moveTile(7);
+        } else if (boxList[6].hitTest(
+          result,
+          position: boxList[6].globalToLocal(details.position),
+        )) {
+          moveTile(6);
+        } else if (boxList[5].hitTest(
+          result,
+          position: boxList[5].globalToLocal(details.position),
+        )) {
+          moveTile(5);
+        } else if (boxList[4].hitTest(
+          result,
+          position: boxList[4].globalToLocal(details.position),
+        )) {
+          moveTile(4);
+        } else if (boxList[3].hitTest(
+          result,
+          position: boxList[3].globalToLocal(details.position),
+        )) {
+          moveTile(3);
+        } else if (boxList[2].hitTest(
+          result,
+          position: boxList[2].globalToLocal(details.position),
+        )) {
+          moveTile(2);
+        } else if (boxList[1].hitTest(
+          result,
+          position: boxList[1].globalToLocal(details.position),
+        )) {
+          moveTile(1);
+        } else if (boxList[0].hitTest(
+          result,
+          position: boxList[0].globalToLocal(details.position),
+        )) {
+          moveTile(0);
+        }
+      },
+      child: Container(
+        color: Colors.transparent,
+        child: Center(
+          child: Text(
+            "${tiles[index] == 16 ? "" : tiles[index]}",
+          ),
         ),
       ),
-      onTap: () {
-        moveTile(index);
-      },
+    );
+  }
+
+  _buildWebTile(int index) {
+    return InkWell(
+      onHover: (val) => moveTile(index),
+      onTap: () => moveTile(index),
+      child: Container(
+        color: Colors.transparent,
+        child: Center(
+          child: Text(
+            "${tiles[index] == 16 ? "" : tiles[index]}",
+          ),
+        ),
+      ),
     );
   }
 }
